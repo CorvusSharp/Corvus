d2824ccd1ffee96ee8ab784eebcc91ecfee66e17
root <root@DESKTOP-EQPBMTF>
2023-05-10 20:37:57 +0300

    OC

--- lab3Y/task13/son1.c
+++ lab3Y/task13/son1.c
@@ -4,11 +4,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+ 
 void main(){
-int pid,ppid;
-pid=getpid();
-ppid=getppid();
-printf("SON_1 PARAMS: pid=%i ppid=%i\nFather creates and waits\
-n",pid,ppid);
-sleep(3);
+    int pid,ppid;
+    pid=getpid();
+    ppid=getppid();
+    printf("SON_1 PARAMS: pid=%i ppid=%i\nFather creates and waits\n",pid,ppid);
+    sleep(3);
 }
--- lab3Y/task13/son3.c
+++ lab3Y/task13/son3.c
@@ -4,12 +4,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-void main(){
-int pid,ppid;
-pid=getpid();
-ppid=getppid();
-printf("SON_1 PARAMS: pid=%i ppid=%i\nFather creates and waits\
-n",pid,ppid);
-sleep(3);
+ 
+int main(){
+	int pid,ppid;
+	pid=getpid();
+	ppid=getppid();
+	printf("SON_3 PARAMS:  pid=%i  ppid=%i\nson3 terminated–ZOMBIE\n",pid,ppid);
+	ppid=getppid();
+	printf("SON_3 PARAMS:  pid=%i  ppid=%i\n",pid,ppid);
+ 
+    return 0;
 }
-
+ 
